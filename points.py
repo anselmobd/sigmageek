5237d44501ffc7d0b01513fd6db5c9fe425831d0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-01-27 21:44:16 -0300

    The default Vector3D is origin

--- points.py
+++ points.py
@@ -16,7 +16,7 @@ from pprint import (
 
 class Vector3D:
 
-    def __init__(self, x, y, z):
+    def __init__(self, x=0, y=0, z=0):
         self.x = float(x)
         self.y = float(y)
         self.z = float(z)
